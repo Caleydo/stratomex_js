16dd86fedd92174bd8b9e92989b86a6490943fcd
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-02-04 16:39:43 -0500

    use layout.DistributeLayout

--- column.js
+++ column.js
@@ -1,29 +1,57 @@
 /**
  * Created by sam on 30.01.2015.
  */
-define(function () {
+define(['require', 'exports', 'd3', '../caleydo/vis', '../caleydo/multiform', '../caleydo/idtype', '../caleydo/behavior'], function (require, exports) {
   var d3 = require('d3');
   var vis = require('../caleydo/vis');
   var multiform = require('../caleydo/multiform');
-  function Cluster(group, data) {
-    this.group = group;
-    this.data = data;
+  var idtypes = require('../caleydo/idtype');
+  var behaviors = require('../caleydo/behavior');
+  var events = require('../caleydo/event');
+  var layouts = require('../caleydo-layout/main');
+
+  function guessInitial(desc) {
+    if (desc.type === 'matrix') {
+      return 'caleydo-vis-heatmap';
+    }
+    if (desc.type === 'vector') {
+      return  desc.value.type === 'categorical' ? 'caleydo-vis-mosaic' : 'caleydo-vis-heatmap1d';
+    }
+    return -1;
   }
-  Cluster.prototype.build = function (parent) {
-    this.multiform = multiform.create(parent, data);
-  };
+
+  var manager = exports.manager = new idtypes.ObjectManager('_column', 'Column');
+
   function Column(parent, data, partitioning) {
     this.data = data;
-    this.$parent = d3.select(parent).append('div').attr('class','column');
+    this.$parent = d3.select(parent).append('div').attr('class', 'column');
     this.partitioning = partitioning;
-    this.$clusters = this.$parent.append('div').classed('clusters',true).selectAll('div.cluster').data(partitioning.groups.map(function (group) {
-      return new Cluster(group, data.view(group));
-    }));
-    this.$clusters.enter().append('div')
-      .attr('class', 'cluster')
-      .each(function(cluster) {
-        cluster.build(this);
-      });
+    var initialVis = guessInitial(data.desc);
+    this.grid = multiform.createGrid(data, partitioning, this.$parent.node(), function (data, range) {
+      return data.view(range)
+    }, {
+      initialVis: initialVis
+    });
+    this.id = manager.nextId(this);
+
+    var z = this.zoom = new behaviors.ZoomLogic(this.grid, this.grid.asMetaData);
+    var layoutOptions = {};
+    var g = this.grid.on('changed', function() {
+      layoutOptions['prefWidth'] = z.isWidthFixed ? g.size.x : Number.NaN;
+      layoutOptions['prefHeight'] = z.isHeightFixed ? g.size.x : Number.NaN;
+      manager.fire('dirty'); //fire relayout
+    });
+    this.layout = layouts.wrapDOM(this.$parent.node(), layoutOptions);
+    manager.fire('dirty'); //fire relayout
   }
-  return Column;
+  Column.prototype.layouted = function() {
+    //sync the scaling
+    var size = this.layout.getSize();
+    this.zoom.zoomTo(size.x, size.y);
+  };
+
+
+  exports.create = function(parent, data, partitioning) {
+    return new Column(parent, data, partitioning);
+  }
 });
--- main.js
+++ main.js
@@ -4,13 +4,20 @@
 define(function (require) {
   'use strict';
   var d3 = require('d3');
+  var $ = require('jQuery');
   var data = require('../caleydo/data');
   var vis = require('../caleydo/vis');
-  var selectionInfo = require('../caleydo-selectioninfo/main');
-  var info = selectionInfo.create(document.getElementById('selectioninfo'));
+  var ranges = require('../caleydo/range');
+
+  var layout = require('../caleydo-layout/main').distributeLayout(true, 100, { top : 30, left: 30, right: 30, bottom: 10});
+  var info = require('../caleydo-selectioninfo/main').create(document.getElementById('selectioninfo'));
+
+  var columns = require('./column.js');
+
+  var stratomex = document.getElementById('stratomex');
   var lineup;
 
-  data.listAsTable(true).then(function (datalist) {
+  function createLineUp(datalist) {
     var v = vis.list(datalist);
     v = v.filter(function (v) { return v.id === 'caleydo-vis-lineup';})[0];
     return v.load().then(function (plugin) {
@@ -29,5 +36,27 @@ define(function (require) {
       });
       return lineup;
     });
+  }
+
+  function listenToData(datalist) {
+    datalist.on('select-selected', function(event, range) {
+      if (range.isNone) {
+        return;
+      }
+      datalist.objects(range).then(function(toAdd) {
+        var m = toAdd[0]._;
+        columns.create(stratomex, m, ranges.range(0));
+      });
+    });
+    return datalist;
+  }
+  data.listAsTable(true).then(listenToData).then(createLineUp);
+
+  columns.manager.on('dirty', function() {
+    //update the layout
+    var w = $(stratomex).width();
+    var h = $(stratomex).height();
+    layout(columns.manager.entries().map(function(c) { return c.layout; }), w, h);
+    columns.manager.forEach(function(c) {c.layouted();});
   });
 });
