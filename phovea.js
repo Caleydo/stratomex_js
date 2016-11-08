b41a6842dacaf556ee61f933638e5e536a5a2ede
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-11-08 11:43:57 +0100

    fix cases in extension points

--- phovea.js
+++ phovea.js
@@ -5,45 +5,59 @@
  **************************************************************************** */
 
 //register all extensions in the registry following the given pattern
-module.exports = function(registry) {
+module.exports = function (registry) {
   //registry.push('extension-type', 'extension-id', function() { return System.import('./src/extension_impl'); }, {});
   // generator-phovea:begin
-  registry.push('view', 'stratomex', function() { return System.import('./src/stratomex'); }, {
-  'location': 'center'
- });
+  registry.push('view', 'stratomex', function () {
+    return System.import('./src/StratomeX');
+  }, {
+    'location': 'center'
+  });
 
-  registry.push('view', 'stratomex-lineup', function() { return System.import('./src/lineup'); }, {
-  'location': 'bottom'
- });
+  registry.push('view', 'stratomex-lineup', function () {
+    return System.import('./src/LineUp');
+  }, {
+    'location': 'bottom'
+  });
 
-  registry.push('actionFactory', 'stratomex-note', function() { return System.import('./src/notes'); }, {
-  'factory': 'createCmd',
-  'creates': '(addStratomeXNote|changeStratomeXNote|removeStratomeXNote)'
- });
+  registry.push('actionFactory', 'stratomex-note', function () {
+    return System.import('./src/notes');
+  }, {
+    'factory': 'createCmd',
+    'creates': '(addStratomeXNote|changeStratomeXNote|removeStratomeXNote)'
+  });
 
-  registry.push('actionFactory', 'stratomex-column', function() { return System.import('./src/Column'); }, {
-  'factory': 'createCmd',
-  'creates': '(createStratomeXColumn|removeStratomeXColumn|swapStratomeXColumns|setStratomeXColumnOption|changeStratomeXColumnVis|showStratomeXInDetail)'
- });
+  registry.push('actionFactory', 'stratomex-column', function () {
+    return System.import('./src/Column');
+  }, {
+    'factory': 'createCmd',
+    'creates': '(createStratomeXColumn|removeStratomeXColumn|swapStratomeXColumns|setStratomeXColumnOption|changeStratomeXColumnVis|showStratomeXInDetail)'
+  });
 
-  registry.push('actionCompressor', 'stratomex-compressSwap', function() { return System.import('./src/Column'); }, {
-  'factory': 'compressSwap',
-  'matches': 'swapStratomeXColumns'
- });
+  registry.push('actionCompressor', 'stratomex-compressSwap', function () {
+    return System.import('./src/Column');
+  }, {
+    'factory': 'compressSwap',
+    'matches': 'swapStratomeXColumns'
+  });
 
-  registry.push('actionCompressor', 'stratomex-compressCreateRemove', function() { return System.import('./src/Column'); }, {
-  'factory': 'compressCreateRemove',
-  'matches': '(createStratomeXColumn|removeStratomeXColumn)'
- });
+  registry.push('actionCompressor', 'stratomex-compressCreateRemove', function () {
+    return System.import('./src/Column');
+  }, {
+    'factory': 'compressCreateRemove',
+    'matches': '(createStratomeXColumn|removeStratomeXColumn)'
+  });
 
-  registry.push('actionCompressor', 'stratomex-compressHideShowDetail', function() { return System.import('./src/Column'); }, {
-  'factory': 'compressHideShowDetail',
-  'matches': 'showStratomeXInDetail'
- });
+  registry.push('actionCompressor', 'stratomex-compressHideShowDetail', function () {
+    return System.import('./src/Column');
+  }, {
+    'factory': 'compressHideShowDetail',
+    'matches': 'showStratomeXInDetail'
+  });
 
-  registry.push('app', 'stratomex_js', function() { return System.import('./src/'); }, {
-  'name': 'Stratomex.js'
- });
+  registry.push('app', 'stratomex_js', null, {
+    'name': 'Stratomex.js'
+  });
   // generator-phovea:end
 };
 
