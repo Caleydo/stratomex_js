8bed8f764b324f32182d83d15aff55a469bcf27c
Klaus Eckelt <Klaus.Eckelt@gmail.com>
2017-07-02 14:40:01 +0200

    Replace type check and duplicate category check n similarity task

--- stratomex_js/tasks.py
+++ stratomex_js/tasks.py
@@ -4,7 +4,6 @@ from phovea_processing_queue.task_definition import task, getLogger
 
 from phovea_server.dataset import get as get_dataset
 from phovea_server.dataset import list_datasets
-from phovea_server.dataset_def import AStratification, AMatrix
 
 _log = getLogger(__name__)
 
@@ -42,7 +41,7 @@ def similarity(datasetId, groupName):
       datasets = list_datasets()
       for dataset in datasets:
         # check data type, e.g. HDFTable, HDFStratification, HDFMatrix
-        if isinstance(dataset, AStratification) and dataset.id != datasetId:
+        if dataset.type == 'stratification':
           for group in dataset.groups():
             #now we have got two list that should get compared
             patSet2 = set(dataset.rowids(group.range))
@@ -55,32 +54,27 @@ def similarity(datasetId, groupName):
               result[dataset.id] = jaccard
 
 
-        if isinstance(dataset, AMatrix):
-          if hasattr(dataset, 'categories'): #some data has no categories (e.g. mRNA, RPPA)
-            _log.info('Start processing matrix '+dataset.id)
-            matData = dataset.asnumpy()
-            # for each column (e.g. gene (e.g. A1CF))
-            # datatset.cols() are the stuff that can be in added to stratomex
-            for col in range(matData.shape[1]):
-              if col % 500 == 0:
-                _log.info('Processing col ' + str(col) + "/" + str(matData.shape[1]))
+        elif dataset.type == 'matrix' and dataset.value == 'categorical': #some matrix data has no categories (e.g. mRNA, RPPA)
+          _log.info('Start processing matrix '+dataset.id)
+          matData = dataset.asnumpy()
+          # for each column (e.g. gene (e.g. A1CF))
+          # datatset.cols() are the stuff that can be in added to stratomex
+          for col in range(matData.shape[1]):
+            if col % 500 == 0:
+              _log.info('Processing col ' + str(col) + "/" + str(matData.shape[1]))
 
-              matColumn = matData[:, col]
-              # check in which categories the patients are
-              if hasattr(dataset, 'categories'):
-                for cat in dataset.categories:
-                  catRowIndicies = [i for i, v in enumerate(matColumn) if v == cat['name']]
-                  patSet2 = set(dataset.rowids()[catRowIndicies])
+            matColumn = matData[:, col]
+            # check in which categories the patients are
+            for cat in dataset.categories:
+              catRowIndicies = [i for i, v in enumerate(matColumn) if v == cat['name']]
+              patSet2 = set(dataset.rowids()[catRowIndicies])
 
-                  jaccard = len(cmpPatients.intersection(patSet2)) / float(len(cmpPatients.union(patSet2)))
-                  # _log.debug('jaccard index for {} is {}'.format(dataset.id + '/' + dataset.cols()[col], str(jaccard)))
+              jaccard = len(cmpPatients.intersection(patSet2)) / float(len(cmpPatients.union(patSet2)))
+              # _log.debug('jaccard index for {} is {}'.format(dataset.id + '/' + dataset.cols()[col], str(jaccard)))
 
-                  if dataset.id not in result or result[dataset.id] < jaccard:
-                    result[dataset.id + '-c' + str(col)] = jaccard
-                    # e.g. result['tcgaGbmSampledMutations-c9408'] = 1
-          else:
-            # Nothing to do
-            _log.info('Dataset '+dataset.id+' has no categories and is ignored.')
+              if dataset.id not in result or result[dataset.id] < jaccard:
+                result[dataset.id + '-c' + str(col)] = jaccard
+                # e.g. result['tcgaGbmSampledMutations-c9408'] = 1
     else:
       _log.error('Group %s not part of column %s.', groupName, datasetId)
       raise Exception('Group '+groupName+' not part of column '+  datasetId+'.')
