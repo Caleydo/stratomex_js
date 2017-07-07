da0b4cdaa0f46effacb1faa105f0c8c13bd96d9d
Klaus Eckelt <Klaus.Eckelt@gmail.com>
2017-07-07 13:09:40 +0200

    removed unnecessary size check in task

--- stratomex_js/tasks.py
+++ stratomex_js/tasks.py
@@ -26,48 +26,43 @@ def similarity(method, ids):
     cmpPatients = np.array(parsedRange[0])  # [0] since ranges are multidimensional but you just want the first one
 
     #compare that group's list of patients to all others
-    if cmpPatients.size > 0: # found it?
-      datasets = list_datasets()
-      for dataset in datasets:
-        # check data type, e.g. HDFTable, HDFStratification, HDFMatrix
-        if dataset.type == 'stratification':
-          for group in dataset.groups():
-            #now we have got two list that should get compared
-            patSet2 = dataset.rowids(group.range)
+    datasets = list_datasets()
+    for dataset in datasets:
+      # check data type, e.g. HDFTable, HDFStratification, HDFMatrix
+      if dataset.type == 'stratification':
+        for group in dataset.groups():
+          #now we have got two list that should get compared
+          patSet2 = dataset.rowids(group.range)
 
-            #jaccard = intersection / union
-            jaccard = np.intersect1d(cmpPatients, patSet2).size / np.union1d(cmpPatients, patSet2).size
-            #_log.debug('jaccard for {} index is {}'.format(dataset.id+'/'+group.name, str(jaccard)))
+          #jaccard = intersection / union
+          jaccard = np.intersect1d(cmpPatients, patSet2).size / np.union1d(cmpPatients, patSet2).size
+          #_log.debug('jaccard for {} index is {}'.format(dataset.id+'/'+group.name, str(jaccard)))
 
-            if dataset.id not in result or result[dataset.id] < jaccard:
-              result[dataset.id] = jaccard
+          if dataset.id not in result or result[dataset.id] < jaccard:
+            result[dataset.id] = jaccard
 
+      elif dataset.type == 'matrix' and dataset.value == 'categorical': #some matrix data has no categories (e.g. mRNA, RPPA)
+        _log.debug('Start processing matrix '+dataset.id)
+        matData = dataset.asnumpy()
 
-        elif dataset.type == 'matrix' and dataset.value == 'categorical': #some matrix data has no categories (e.g. mRNA, RPPA)
-          _log.info('Start processing matrix '+dataset.id)
-          matData = dataset.asnumpy()
+        # for each column (e.g. gene (e.g. A1CF))
+        # datatset.cols() are the stuff that can be in added to stratomex
+        for col in range(matData.shape[1]):
+          if col % 500 == 0:
+            _log.debug('Processing col ' + str(col) + "/" + str(matData.shape[1]))
 
-          # for each column (e.g. gene (e.g. A1CF))
-          # datatset.cols() are the stuff that can be in added to stratomex
-          for col in range(matData.shape[1]):
-            if col % 500 == 0:
-              _log.info('Processing col ' + str(col) + "/" + str(matData.shape[1]))
+          matColumn = matData[:, col]
+          # check in which categories the patients are
+          for cat in dataset.categories:
+            catRowIndicies = np.argwhere(matColumn == cat['name'])[:,0] #get indicies as 1column matrix and convert to 1d array
+            patientsInCat = dataset.rowids()[catRowIndicies] #indicies to patient ids
+            jaccard = np.intersect1d(cmpPatients, patientsInCat).size / np.union1d(cmpPatients, patientsInCat).size
+            # _log.debug('jaccard index for {} is {}'.format(dataset.id + '/' + dataset.cols()[col], str(jaccard)))
 
-            matColumn = matData[:, col]
-            # check in which categories the patients are
-            for cat in dataset.categories:
-              catRowIndicies = np.argwhere(matColumn == cat['name'])[:,0] #get indicies as 1column matrix and convert to 1d array
-              patientsInCat = dataset.rowids()[catRowIndicies] #indicies to patient ids
-              jaccard = np.intersect1d(cmpPatients, patientsInCat).size / np.union1d(cmpPatients, patientsInCat).size
-              # _log.debug('jaccard index for {} is {}'.format(dataset.id + '/' + dataset.cols()[col], str(jaccard)))
-
-              columnId = dataset.id + '-c' + str(col)
-              if columnId not in result or result[columnId] < jaccard:
-                result[columnId] = jaccard
-                # e.g. result['tcgaGbmSampledMutations-c9408'] = 1
-    else:
-      _log.error('Group %s not part of column %s.', groupName, datasetId)
-      raise Exception('Group '+groupName+' not part of column '+  datasetId+'.')
+            columnId = dataset.id + '-c' + str(col)
+            if columnId not in result or result[columnId] < jaccard:
+              result[columnId] = jaccard
+              # e.g. result['tcgaGbmSampledMutations-c9408'] = 1
 
     _log.debug('Done with Jaccard calculation.')
   except Exception as e:
