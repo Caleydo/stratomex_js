57eae4bb32c17a48b5a45698d22dd9c1a2523934
Klaus Eckelt <Klaus.Eckelt@gmail.com>
2017-07-07 20:38:40 +0200

    Fix flake8 erros II

--- stratomex_js/tasks.py
+++ stratomex_js/tasks.py
@@ -21,8 +21,8 @@ def similarity(method, ids):
 
   try:
     from phovea_server.range import parse
-    parsedRange = parse(ids)
-    cmpPatients = np.array(parsedRange[0])  # [0] since ranges are multidimensional but you just want the first one
+    parsed_range = parse(ids)
+    cmp_patients = np.array(parsed_range[0])  # [0] since ranges are multidimensional but you just want the first one
 
     # compare that group's list of patients to all others
     datasets = list_datasets()
@@ -31,10 +31,10 @@ def similarity(method, ids):
       if dataset.type == 'stratification':
         for group in dataset.groups():
           # now we have got two list that should get compared
-          patSet2 = dataset.rowids(group.range)
+          pat_set2 = dataset.rowids(group.range)
 
           # jaccard = intersection / union
-          jaccard = np.intersect1d(cmpPatients, patSet2).size / np.union1d(cmpPatients, patSet2).size
+          jaccard = np.intersect1d(cmp_patients, pat_set2).size / np.union1d(cmp_patients, pat_set2).size
           # _log.debug('jaccard for {} index is {}'.format(dataset.id+'/'+group.name, str(jaccard)))
 
           if dataset.id not in result or result[dataset.id] < jaccard:
@@ -42,41 +42,41 @@ def similarity(method, ids):
 
       elif dataset.type == 'matrix' and dataset.value == 'categorical':  # some matrix data has no categories (e.g. mRNA, RPPA)
         _log.debug('Start processing matrix ' + dataset.id)
-        matData = dataset.asnumpy()
+        mat_data = dataset.asnumpy()
 
         # for each column (e.g. gene (e.g. A1CF))
         # datatset.cols() are the stuff that can be in added to stratomex
-        for col in range(matData.shape[1]):
+        for col in range(mat_data.shape[1]):
           if col % 500 == 0:
-            _log.debug('Processing col ' + str(col) + "/" + str(matData.shape[1]))
+            _log.debug('Processing col ' + str(col) + "/" + str(mat_data.shape[1]))
 
-          matColumn = matData[:, col]
+          mat_column = mat_data[:, col]
           # check in which categories the patients are
           for cat in dataset.categories:
-            catRowIndicies = np.argwhere(matColumn == cat['name'])[:, 0]  # get indicies as 1column matrix and convert to 1d array
-            patientsInCat = dataset.rowids()[catRowIndicies]  # indicies to patient ids
-            jaccard = np.intersect1d(cmpPatients, patientsInCat).size / np.union1d(cmpPatients, patientsInCat).size
+            cat_row_indicies = np.argwhere(mat_column == cat['name'])[:, 0]  # get indicies as 1column matrix and convert to 1d array
+            patients_in_cat = dataset.rowids()[cat_row_indicies]  # indicies to patient ids
+            jaccard = np.intersect1d(cmp_patients, patients_in_cat).size / np.union1d(cmp_patients, patients_in_cat).size
             # _log.debug('jaccard index for {} is {}'.format(dataset.id + '/' + dataset.cols()[col], str(jaccard)))
 
-            columnId = dataset.id + '-c' + str(col)
-            if columnId not in result or result[columnId] < jaccard:
-              result[columnId] = jaccard
+            column_id = dataset.id + '-c' + str(col)
+            if column_id not in result or result[column_id] < jaccard:
+              result[column_id] = jaccard
               # e.g. result['tcgaGbmSampledMutations-c9408'] = 1
 
       elif dataset.type == 'table':
         for col in dataset.columns:
           if col.type == 'categorical':
-            colData = col.asnumpy()  # table doesnt have asnumpy()
+            col_data = col.asnumpy()  # table doesnt have asnumpy()
             for cat in col.categories:
-              catName = cat if isinstance(cat, str) else cat['name']  # TCGA table had just the strings, calumma table has a dict like matrix above
-              catRowIndicies = np.argwhere(colData == catName)[:, 0]
-              if catRowIndicies.size > 0:
-                patientsInCat = dataset.rowids()[catRowIndicies]  # indicies to patient ids
-                jaccard = np.intersect1d(cmpPatients, patientsInCat).size / np.union1d(cmpPatients, patientsInCat).size
+              cat_name = cat if isinstance(cat, str) else cat['name']  # TCGA table had just the strings, calumma table has a dict like matrix above
+              cat_row_indicies = np.argwhere(col_data == cat_name)[:, 0]
+              if cat_row_indicies.size > 0:
+                patients_in_cat = dataset.rowids()[cat_row_indicies]  # indicies to patient ids
+                jaccard = np.intersect1d(cmp_patients, patients_in_cat).size / np.union1d(cmp_patients, patients_in_cat).size
 
-                columnId = dataset.id + '_' + col.name  # id in stratomex has trailing '-s' which is not needed here (e.g. tcgaGbmSampledClinical_patient.ethnicity-s)
-                if columnId not in result or result[columnId] < jaccard:
-                  result[columnId] = jaccard
+                column_id = dataset.id + '_' + col.name  # id in stratomex has trailing '-s' which is not needed here (e.g. tcgaGbmSampledClinical_patient.ethnicity-s)
+                if column_id not in result or result[column_id] < jaccard:
+                  result[column_id] = jaccard
 
   except Exception as e:
     _log.exception('Can not fulfill task. Error: %s.', e)
