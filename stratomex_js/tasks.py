7eb0eff2cc2cd6701c8834b12dc9d877035849f0
Klaus Eckelt <Klaus.Eckelt@gmail.com>
2017-07-07 20:00:17 +0200

    Fix flake8 erros

--- stratomex_js/tasks.py
+++ stratomex_js/tasks.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, division
 
 from phovea_processing_queue.task_definition import task, getLogger
 
-from phovea_server.dataset import get as get_dataset
 from phovea_server.dataset import list_datasets
 import numpy as np
 
@@ -25,24 +24,24 @@ def similarity(method, ids):
     parsedRange = parse(ids)
     cmpPatients = np.array(parsedRange[0])  # [0] since ranges are multidimensional but you just want the first one
 
-    #compare that group's list of patients to all others
+    # compare that group's list of patients to all others
     datasets = list_datasets()
     for dataset in datasets:
       # check data type, e.g. HDFTable, HDFStratification, HDFMatrix
       if dataset.type == 'stratification':
         for group in dataset.groups():
-          #now we have got two list that should get compared
+          # now we have got two list that should get compared
           patSet2 = dataset.rowids(group.range)
 
-          #jaccard = intersection / union
+          # jaccard = intersection / union
           jaccard = np.intersect1d(cmpPatients, patSet2).size / np.union1d(cmpPatients, patSet2).size
-          #_log.debug('jaccard for {} index is {}'.format(dataset.id+'/'+group.name, str(jaccard)))
+          # _log.debug('jaccard for {} index is {}'.format(dataset.id+'/'+group.name, str(jaccard)))
 
           if dataset.id not in result or result[dataset.id] < jaccard:
             result[dataset.id] = jaccard
 
-      elif dataset.type == 'matrix' and dataset.value == 'categorical': #some matrix data has no categories (e.g. mRNA, RPPA)
-        _log.debug('Start processing matrix '+dataset.id)
+      elif dataset.type == 'matrix' and dataset.value == 'categorical':  # some matrix data has no categories (e.g. mRNA, RPPA)
+        _log.debug('Start processing matrix ' + dataset.id)
         matData = dataset.asnumpy()
 
         # for each column (e.g. gene (e.g. A1CF))
@@ -54,8 +53,8 @@ def similarity(method, ids):
           matColumn = matData[:, col]
           # check in which categories the patients are
           for cat in dataset.categories:
-            catRowIndicies = np.argwhere(matColumn == cat['name'])[:,0] #get indicies as 1column matrix and convert to 1d array
-            patientsInCat = dataset.rowids()[catRowIndicies] #indicies to patient ids
+            catRowIndicies = np.argwhere(matColumn == cat['name'])[:, 0]  # get indicies as 1column matrix and convert to 1d array
+            patientsInCat = dataset.rowids()[catRowIndicies]  # indicies to patient ids
             jaccard = np.intersect1d(cmpPatients, patientsInCat).size / np.union1d(cmpPatients, patientsInCat).size
             # _log.debug('jaccard index for {} is {}'.format(dataset.id + '/' + dataset.cols()[col], str(jaccard)))
 
@@ -67,21 +66,20 @@ def similarity(method, ids):
       elif dataset.type == 'table':
         for col in dataset.columns:
           if col.type == 'categorical':
-            colData = col.asnumpy() #table doesnt have asnumpy()
+            colData = col.asnumpy()  # table doesnt have asnumpy()
             for cat in col.categories:
-              catName = cat if isinstance(cat, str) else cat['name'] #TCGA table had just the strings, calumma table has a dict like matrix above
-              catRowIndicies = np.argwhere(colData == catName)[:,0]
+              catName = cat if isinstance(cat, str) else cat['name']  # TCGA table had just the strings, calumma table has a dict like matrix above
+              catRowIndicies = np.argwhere(colData == catName)[:, 0]
               if catRowIndicies.size > 0:
                 patientsInCat = dataset.rowids()[catRowIndicies]  # indicies to patient ids
                 jaccard = np.intersect1d(cmpPatients, patientsInCat).size / np.union1d(cmpPatients, patientsInCat).size
 
-                columnId = dataset.id + '_' + col.name #id in stratomex has trailing '-s' which is not needed here (e.g. tcgaGbmSampledClinical_patient.ethnicity-s)
+                columnId = dataset.id + '_' + col.name  # id in stratomex has trailing '-s' which is not needed here (e.g. tcgaGbmSampledClinical_patient.ethnicity-s)
                 if columnId not in result or result[columnId] < jaccard:
                   result[columnId] = jaccard
 
   except Exception as e:
     _log.exception('Can not fulfill task. Error: %s.', e)
-    raise #rejects promise
+    raise  # rejects promise
 
-  return result #to JSON automatically
-
+  return result  # to JSON automatically
